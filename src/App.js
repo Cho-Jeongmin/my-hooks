e54f7a841d1a658960f98a03eeddac21a397dac5
Jeongmin Cho <jojm0829@gmail.com>
2023-02-11 18:18:45 +0900

    #1.2 useInput part Two

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    setValue(event.target.value);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
@@ -17,31 +27,5 @@ const App = () => {
     </div>
   );
 };
-
-class AppUgly extends React.Component {
-  state = {
-    item: 1,
-  };
-  incrementItem = () => {
-    this.setState((current) => {
-      return { item: current.item + 1 };
-    });
-  };
-  decrementItem = () => {
-    this.setState((current) => {
-      return { item: current.item - 1 };
-    });
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
-  }
-}
 
 export default App;
