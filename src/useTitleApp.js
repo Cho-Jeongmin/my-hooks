b950da602610775aa500dc0df4c63df26a30bcba
Jeongmin Cho <jojm0829@gmail.com>
2023-02-12 16:23:13 +0900

    #2.1 useTitle

---
+++ src/useTitleApp.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 3000);
+  return <div className="App">Hi</div>;
+}
+
+export default App;
