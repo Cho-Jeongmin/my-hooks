6db580091c2d870d59f6d18e0f3fc43961b082e7
Jeongmin Cho <jojm0829@gmail.com>
2023-02-16 01:47:58 +0900

    #2.5 useFadeIn & useNetwork

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./useFadeIn";
+import App from "./useNetwork";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ src/useNetwork.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, useRef } from "react";
+
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine); //사용자정의함수 실행
+    }
+    setStatus(navigator.onLine); //status를 현재 네트워크 상태로 갱신
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    return () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+  return status;
+};
+
+function App() {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "we are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
+    </div>
+  );
+}
+
+export default App;
