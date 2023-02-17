0c0d8e58331a1341e56755f7ab9f4d8e9be7524d
Jeongmin Cho <jojm0829@gmail.com>
2023-02-17 23:57:13 +0900

    #2.6 useScroll

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./useNetwork";
+import App from "./useScroll";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
