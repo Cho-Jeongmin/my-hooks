6caa0ca144cb7470032a669e4ad29f13f50acab1
Jeongmin Cho <jojm0829@gmail.com>
2023-02-18 00:33:09 +0900

    #2.6 useScroll & useFullscreen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./useScroll";
+import App from "./useFullscreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ src/useFullscreen.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from "react";
+
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullscreen) {
+        element.current.mozRequestFullscreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+      runCb(true);
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
+
+function App() {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div ref={element}>
+      <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
+      <button onClick={triggerFull}>Make fullscreen</button>
+      <button onClick={exitFull}>Exit fullscreen</button>
+    </div>
+  );
+}
+
+export default App;
