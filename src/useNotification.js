46734417679bda8db90483299781444f7d443a20
Jeongmin Cho <jojm0829@gmail.com>
2023-02-18 01:35:08 +0900

    #2.7 useNotification

---
+++ src/useNotification.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, useRef } from "react";
+
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+function App() {
+  const triggerNotif = useNotification("Can I steal your kimchi?", {
+    body: "I love kimchi don't you",
+  });
+  return (
+    <div>
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+}
+
+export default App;
