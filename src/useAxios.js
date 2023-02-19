9e1af23f5344021ba5f7e5041071d0bddbd6dd44
Jeongmin Cho <jojm0829@gmail.com>
2023-02-20 00:05:46 +0900

    #2.8 useAxios

--- src/useAxios.js
+++ src/useAxios.js
@@ -4,10 +4,14 @@ import { useState, useEffect } from "react";
 const useAxios = (opts, axiosInstance = defaultAxios) => {
   const [state, setState] = useState({
     loading: true,
-    error: null,
     data: null,
+    error: null,
   });
-
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({ ...state, loading: true });
+    setTrigger(Date.now()); //겹치지 않는 랜덤숫자 발생
+  };
   useEffect(() => {
     if (!opts.url) {
       return;
@@ -15,7 +19,7 @@ const useAxios = (opts, axiosInstance = defaultAxios) => {
     axiosInstance(opts)
       .then((data) => {
         setState({
-          ...state,
+          ...state, //...state는 기존것은 그대로 두기 위함
           loading: false,
           data,
         });
@@ -23,16 +27,22 @@ const useAxios = (opts, axiosInstance = defaultAxios) => {
       .catch((error) => {
         setState({ ...state, loading: false, error });
       });
-  }, []);
-  return state;
+  }, [trigger]);
+  return { ...state, refetch };
 };
 
 function App() {
-  const { loading, data, error } = useAxios({
+  const { loading, data, error, refetch } = useAxios({
     url: "https://yts.mx/api/v2/list_movies.json",
   });
   console.log(`Loding: ${loading}, Data: ${data}, Error: ${error}`);
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
 }
 
 export default App;
